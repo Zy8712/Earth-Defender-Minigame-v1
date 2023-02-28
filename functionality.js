dad56b094a22a6deb87a56daa1b07bb569c4637f
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2023-02-28 02:39:15 -0500

    Add files via upload

--- functionality.js
+++ functionality.js
@@ -1,13 +1,13 @@
 var boxCounter = 0;
 var start = false;
+var earthCounterTotal = 0;
+var earthRemaining = 0;
 window.onload = function(){
   createBoxes();
   $("myButton").observe("click", createBoxes);
 }
 
-function createBoxes()
-{
-//  var colors = ["red", "green", "blue", "silver", "orange"];
+function createBoxes(){
   var planets = ["./Planets/Ice.png", "./Planets/Lava.png", "./Planets/Terran.png", "./Planets/Baren.png", "./Planets/Black_hole.png"];
   for(var i = 0; i < 100; i++){
     var newP = document.createElement("p");
@@ -18,14 +18,18 @@ function createBoxes()
     var left = randomY + "px";
     newP.style.top = top;
     newP.style.left = left;
-    newImg.src = planets[Math.floor(Math.random() * 5)];
+    var randomNum = Math.floor(Math.random() * 5);
+    if(randomNum == 2){
+      earthCounterTotal++;
+      earthRemaining++;
+    }
+    newImg.src = planets[randomNum];
     newP.appendChild(newImg);
-//    newP.style.backgroundColor = colors[ Math.floor(Math.random() * 5)];
     newP.observe("mouseover", removeBox);
     $("container").appendChild(newP);
   }
   boxCounter += 100;
-  $("count").innerHTML = "Boxes Left: " +boxCounter;
+  $("count").innerHTML = "Boxes Left: " +boxCounter +", Earths Remaining: " +earthRemaining;
 }
 
 function removeBox(){
@@ -35,6 +39,10 @@ function removeBox(){
     alert("Finished!");
   }
   boxCounter--;
-  $("count").innerHTML = "Boxes Left: " +boxCounter;
+  var image = this.getElementsByTagName("img")[0];
+  if(image.getAttribute("src") == "./Planets/Terran.png"){
+    earthRemaining--;
+  }
+  $("count").innerHTML = "Boxes Left: " +boxCounter +", Earths Remaining: " +earthRemaining;
   $("container").removeChild(this);
 }
